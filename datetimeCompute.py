8e4b6eeb62bdacaa5c3c7b4bc3617fe25823d97c
Xuefen <37746681+Sprinter1999@users.noreply.github.com>
2019-04-09 13:58:20 +0800

    Add files via upload

--- datetimeCompute.py
+++ datetimeCompute.py
@@ -1,16 +1,20 @@
 import datetime
 import re
-#import numpy
-str2=''#表征含有你所说的x，y，z的值
-a=datetime.datetime#startTime
-str2=(str)(datetime.datetime.now())#这个now是你的第二个参数transTime
-match = re.search(r'\d{1,2}:\d{1,2}:\d{1,2}',str2)
-pattern=''
-if(match):
-    pattern=match.group(0)
-lis=[]
-lis=re.split(r':',pattern)
-Hour=(int)(lis[0])
-Min=(int)(lis[1])
-#Sec=(int)(lis[2])
-a=(a+datetime.timedelta(minutes=Hour*60+Min)).strftime('%Y-%m-%d %H:%M')
+a=datetime.datetime.now()
+b=datetime.datetime.now()-datetime.timedelta(minutes=16000)
+def change_time(a: datetime, b:datetime):
+    str2=''#表征含有你所说的x，y，z的值
+    #a=datetime.datetime#startTime
+    str2=(str)(b)#这个now是你的第二个参数transTime
+    match = re.search(r'\d{1,2}:\d{1,2}:\d{1,2}',str2)
+    pattern=''
+    if(match):
+        pattern=match.group(0)
+    lis=[]
+    lis=re.split(r':',pattern)
+    Hour=(int)(lis[0])
+    Min=(int)(lis[1])
+    #Sec=(int)(lis[2])
+    a=(a+datetime.timedelta(minutes=Hour*60+Min)).strftime('%Y-%m-%d %H:%M')
+    return a
+print(change_time(a,b))
